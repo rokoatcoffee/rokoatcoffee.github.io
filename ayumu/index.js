d581df7776db7cad08f9ae109bc408310730331e
roko@bebop <roko.mislov.m@gmail.com>
2024-12-26 20:51:02 +0100

    remake ayumu

--- ayumu/index.js
+++ ayumu/index.js
@@ -1,102 +1,157 @@
-let full_array = [9, 8, 7, 6, 5, 4, 3, 2, 1];
-const timeout = 2000; // milliseconds
-let start_time = 0;
-
-const message = document.getElementById('game-message');
-const play_button = document.getElementById('play-button');
-play_button.style.display = 'block';
-
-const gen_array = function (number_of_elements, start = 0) {
-  let array = [];
-
-  while (array.length < number_of_elements) {
-    array.push(start++);
+class Timer {
+  constructor() {
+    this.__start = null;
+    this.__end = null;
   }
 
-  return array;
-}
+  __now() {
+    return performance.now()
+  }
 
-const set_cell = function (position, value) {
-  document.getElementById(position).innerHTML = value;
-}
+  __format_time() {
+    if (this.__end === null) return "";
+    const duration = (this.__end - this.__start) / 1000;
+    return duration.toFixed(3);
+  }
 
-const clear_cells = function () {
-  for (let i = 0; i < 60; i++) {
-    let cell = document.getElementById(i);
-    cell.innerHTML = '';
-    cell.classList.remove('cover');
-    cell.removeEventListener('click', play_game);
+  start() {
+    this.__start = this.__now();
+  }
+
+  perf() {
+    this.__end = this.__now();
+    const format = this.__format_time();
+    this.__start = null;
+    this.__end = null;
+    return format;
   }
 }
 
-const shuffle = function (array) {
-  //Fisher-Yates shuffle
-  for (let i = array.length - 1; i > 0; i--) {
-    let j = Math.floor(Math.random() * (i + 1));
-    [array[i], array[j]] = [array[j], array[i]];
+
+class Game {
+  constructor(timer) {
+    this.__ids = [];
+    this.__timeout = 2000;
+    this.__timer = timer;
+
+    this.__message = document.getElementById("message");
+    this.__message_txt = "Press the circle to play";
+
+    this.__button = document.getElementById("play");
+    this.__button.style.display = 'block';
   }
 
-  return array;
-}
-
-function toggle_button() {
-  play_button.style.display = (play_button.style.display == 'block') ? 'none' : 'block';
-}
-
-const set_game = function () {
-  message.innerText = '';
-  full_array = [9, 8, 7, 6, 5, 4, 3, 2, 1];
-  clear_cells();
-  values = shuffle(gen_array(9, 1));
-  position = shuffle(gen_array(60)).slice(0, 9);
-
-  for (let i = 0; i < 9; i++) {
-    set_cell(position[i], values[i]);
-  }
-
-  toggle_button();
-
-  setTimeout(() => {
-    for (let i = 0; i < 9; i++) {
-      let cell = document.getElementById(position[i])
-      cell.classList.add('cover');
-      cell.addEventListener('click', play_game);
+  __shuffle(array) {
+    //Fisher-Yates shuffle
+    for (let i = array.length - 1; i > 0; i--) {
+      let j = Math.floor(Math.random() * (i + 1));
+      [array[i], array[j]] = [array[j], array[i]];
     }
 
-    start_time = performance.now();
-  }, timeout);
-
-}
-
-const reset_game = function () {
-  clear_cells();
-  toggle_button();
-}
-
-const format_time = function (start, stop) {
-  let duration = (stop - start) / 1000;
-  return duration.toFixed(3);
-}
-
-const play_game = function () {
-  let clicked_number = parseInt(event.target.innerHTML, 10);
-  let valid_number = full_array.pop();
-
-  if (valid_number == clicked_number) {
-    event.target.innerHTML = '';
-    event.target.classList.remove('cover');
-    event.target.removeEventListener('click', play_game);
-
-    if (full_array.length == 0) {
-      let stop_time = performance.now();
-      message.innerText = `You Win!! Your time is ${format_time(start_time, stop_time)} s.`;
-      reset_game();
-    }
-  } else {
-    let stop_time = performance.now();
-    message.innerHTML = `You Lost :( Your time is ${format_time(start_time, stop_time)} s.`;
-    reset_game();
+    return array;
   }
-}
 
-play_button.addEventListener('click', function () { set_game() });
+  __generate(number_of_elements, start = 0) {
+      let array = [];
+      while (array.length < number_of_elements) {
+        array.push(start++);
+      }
+
+      return array;
+  }
+
+  __set_cell(id, value) {
+    document.getElementById(id).innerHTML = value;
+  }
+
+  __clear_listeners() {
+    this.__ids.forEach(id => {
+      let cell = document.getElementById(id);
+      cell.classList.remove('cover');
+      cell.removeEventListener('click', this.__callback);
+    });
+  }
+
+  __clear_text() {
+    this.__ids.forEach(id => {
+      let cell = document.getElementById(id);
+      cell.innerHTML = '';
+    });
+  }
+
+  __switch() {
+    this.__button.style.display = this.__button.style.display === 'block' ? 'none' : 'block';
+  }
+
+  __callback = (event) => this.__play(event);
+
+  __end_win() {
+    this.__message.innerText = `You Win!! Your time is ${this.__timer.perf()} s.`;
+    this.__switch();
+  }
+
+  __end_lose() {
+    this.__message.innerHTML = `You Lost :( Your time is ${this.__timer.perf()} s.`;
+    this.__clear_listeners();
+
+    setTimeout(() => {
+      this.__clear_text();
+      this.__switch();
+      this.__ids = [];
+      this.__message.innerHTML = this.__message_txt;
+    }, this.__timeout)
+  }
+
+
+  __play(event) {
+    let clicked_number = parseInt(event.target.id, 10);
+    let valid_number = this.__ids.pop();
+
+    if (valid_number == clicked_number) {
+      event.target.classList.remove('cover');
+      event.target.removeEventListener('click', this.__callback);
+      event.target.innerHTML = '';
+
+      if (this.__ids.length == 0) {
+        this.__end_win();
+      }
+
+      return;
+    }
+
+    this.__ids.push(valid_number);
+    this.__end_lose();
+  };
+
+  __set() {
+    let positions = this.__generate(60);
+    this.__ids = this.__shuffle(positions).slice(0, 9);
+
+    let i = 9;
+    this.__ids.forEach(id => {
+      let cell = document.getElementById(id);
+      cell.innerHTML = i;
+      i--;
+    });
+
+    this.__switch();
+
+    setTimeout(() => {
+      this.__ids.forEach(id => {
+        let cell = document.getElementById(id)
+        cell.classList.add('cover');
+        cell.addEventListener('click', this.__callback);
+      });
+      this.__timer.start();
+    }, this.__timeout);
+  };
+
+  listen() {
+    this.__button.addEventListener('click', () => this.__set());
+  };
+};
+
+
+let timer = new Timer();
+let game = new Game(timer);
+game.listen();
